b3f3339343e204e5b07b5e838a75d1d5ccd9ec88
houssine <houssine.bakkali@gmail.com>
2017-02-21 16:25:15 +0100

    [ADD] moving odoo cooperative modules on github

---
+++ easy_my_coop/migrations/8.0.1.0/pre-migration.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from openerp.openupgrade import openupgrade
+import logging
+
+logger = logging.getLogger('OpenUpgrade')
+
+column_renames = {
+    'job_sync_line': [
+        ('adresse', 'address'),
+        ('ville', 'city'),
+        ('codepostal', 'zip'),
+        ('sync_date','date'),
+        ],
+    }
+
+tables_renames = [
+    ('job_sync_line','subscription_request'),
+    ('job_sync',None),
+    ('external_db',None),
+]
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    if not version:
+        return
+    
+    openupgrade.rename_columns(cr, column_renames)
+    openupgrade.rename_tables(cr, tables_renames)
+    
---
+++ easy_my_coop/models/__init__.py
@@ -0,0 +1,9 @@
+from . import coop
+from . import operation_request
+from . import product
+from . import partner
+from . import account_invoice
+from . import company
+from . import res_partner_bank
+from . import account_journal
+#import mail_mail
