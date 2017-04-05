03b5a4ec62e736bb8315f30de81b4d8688bb4f9b
houssine <houssine.bakkali@gmail.com>
2017-04-05 16:01:57 +0200

    [IMP] change company type for french localization

--- easy_my_coop_fr/models/coop.py
+++ easy_my_coop_fr/models/coop.py
@@ -6,6 +6,11 @@ from openerp import api, fields, models, _
 class subscription_request(models.Model):
     _inherit = 'subscription.request'
     
+    company_type = fields.Selection([('asso','Association'),
+                                     ('eurl','EURL / Entreprise individuelle'),
+                                     ('sarl','SARL'),
+                                     ('sa','SA / SAS')])
+    
     def get_required_field(self):
         required_fields = super(subscription_request,self).get_required_field()
 #         if 'no_registre' in required_fields:
