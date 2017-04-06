58ef10818f9284f93ff2d07b5e3785e192d93840
houssine <houssine.bakkali@gmail.com>
2017-04-06 16:49:16 +0200

    [FIX] override function to not send capital release request by mail in case of the payment is online

--- easy_my_coop_online_payment/models/coop.py
+++ easy_my_coop_online_payment/models/coop.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 from openerp import api, fields, models, _
 
-class subscription_request(models.Model):
+class SubscriptionRequest(models.Model):
     _inherit = 'subscription.request'
 
     payment_type = fields.Selection([('online', 'Online'),
                                      ('deferred', 'Deferred')], string='Payment Type')
+    
+    def send_capital_release_request(self, invoice):
+        if self.payment_type == 'deferred':
+            super(SubscriptionRequest, self).send_capital_release_request(invoice)
+        return True
