a4fd152eec2c056c728d243fedd213e97d66b75d
houssine <houssine.bakkali@gmail.com>
2018-02-02 14:34:53 +0100

    [IMP] move the flag become client to easy_my_coop_base

--- easy_my_coop_eater/models/product.py
+++ easy_my_coop_eater/models/product.py
@@ -4,5 +4,4 @@ from openerp import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    customer = fields.Boolean(string='Become customer')
     eater = fields.Selection([('eater', 'Eater'), ('worker_eater', 'Worker and Eater')], string="Eater/Worker")
