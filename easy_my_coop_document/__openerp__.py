3c1a6c0efc007f302f4cda79c7de1c74e455a4fd
Rémy Taymans <remytaymans@gmail.com>
2018-03-17 13:38:20 +0100

    [ADD] emc_document: Management of documents
    Allow you to store documents and organize them in categories.
    
    Access right is not managed yet. It must be solved in further commit.

---
+++ easy_my_coop_document/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Rémy Taymans <remytaymans@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Easy My Coop Document',
+
+    'summary': """
+    Manage the documents of your cooperative.
+    """,
+    'description': """
+    """,
+
+    'author': 'Rémy Taymans',
+    'license': 'AGPL-3',
+    'version': '9.0.1.0',
+    'website': "https://github.com/houssine78/vertical-cooperative",
+
+    'category': 'Cooperative Management',
+
+    'depends': [
+        'base',
+        'web',
+    ],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/easy_my_coop_document_menu.xml',
+        'views/easy_my_coop_document_views.xml',
+    ]
+}
