275a0d0dacfc0126a7ea3ed07df2933fc706dc7b
houssine <houssine@DESKTOP-ICALFFT.lan>
2018-06-01 12:39:03 +0200

    [IMP] override company type selection for swiss

--- easy_my_coop_ch/models/coop.py
+++ easy_my_coop_ch/models/coop.py
@@ -6,11 +6,15 @@ from openerp import api, fields, models, _
 class subscription_request(models.Model):
     _inherit = 'subscription.request'
     
-#     company_type = fields.Selection([('asso','Association'),
-#                                      ('eurl','EURL / Entreprise individuelle'),
-#                                      ('sarl','SARL'),
-#                                      ('sa','SA / SAS')])
-    
+    company_type = fields.Selection([('ei','Individual company'),
+                                     ('snc','Partnership'),
+                                     ('sa','Limited company (SA)'),
+                                     ('sarl','Limited liability company (Ltd)'),
+                                     ('sc','Cooperative'),
+                                     ('asso','Association'),
+                                     ('fond','Foundation'),
+                                     ('edp','Company under public law')])
+        
 #     def get_required_field(self):
 #         required_fields = super(subscription_request,self).get_required_field()
 #         if 'iban' in required_fields:
@@ -19,5 +23,5 @@ class subscription_request(models.Model):
 #         return required_fields
 
     def check_belgian_identification_id(self, nat_register_num):
-        #deactivate number validation for french localization
+        #deactivate number validation for swiss localization
         return True
