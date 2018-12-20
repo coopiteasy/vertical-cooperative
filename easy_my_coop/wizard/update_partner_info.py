f4d40ec2058e4e9f9cead4ffeb1e87afb8cf61fb
houssine <houssine@DESKTOP-ICALFFT>
2018-12-20 14:45:37 +0100

    [PEP8] comply to pep8

--- easy_my_coop/wizard/update_partner_info.py
+++ easy_my_coop/wizard/update_partner_info.py
@@ -3,6 +3,7 @@
 from openerp import api, fields, models, _
 from openerp.exceptions import UserError
 
+
 class PartnerUpdateInfo(models.TransientModel):
     _name = "partner.update.info"
     _description = "Update Partner Info"
@@ -11,7 +12,7 @@ class PartnerUpdateInfo(models.TransientModel):
     def _get_partner(self):
         active_id = self.env.context.get('active_id')
         return self.env['res.partner'].browse(active_id)
-    
+
     @api.model
     def _get_register_number(self):
         partner = self._get_partner()
@@ -19,28 +20,33 @@ class PartnerUpdateInfo(models.TransientModel):
             return partner.company_register_number
         else:
             return partner.national_register_number
-    
-    register_number = fields.Char(string="Register Number", required=True, default=_get_register_number)
-    cooperator = fields.Many2one('res.partner', string="Cooperator", default=_get_partner)
-    
+
+    register_number = fields.Char(string="Register Number",
+                                  required=True,
+                                  default=_get_register_number)
+    cooperator = fields.Many2one('res.partner',
+                                 string="Cooperator",
+                                 default=_get_partner)
+
     def check_belgian_ident_id(self, register_number):
-        if self.env['subscription.request'].check_belgian_identification_id(register_number):
+        if self.env['subscription.request'].check_belgian_identification_id(
+                                                register_number):
             return True
         else:
             raise UserError(_("The national register number is not valid."))
 
     @api.multi
     def update(self):
-        
+
         cooperator = self.cooperator
         coop_vals = {}
-        
+
         if cooperator.is_company:
             coop_vals['company_register_number'] = self.register_number
         else:
             if self.check_belgian_ident_id(self.register_number):
                 coop_vals['national_register_number'] = self.register_number
-        
+
         if coop_vals:
             cooperator.write(coop_vals)
 
