19a8e1f88576851217125fa9fc397e376461c9b8
houssine <houssine@DESKTOP-ICALFFT>
2019-01-21 17:27:18 +0100

    [PEP8] make the code compliant with PEP8

--- easy_my_coop/models/product.py
+++ easy_my_coop/models/product.py
@@ -4,7 +4,7 @@ from openerp import api, fields, models
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    
+
     is_share = fields.Boolean(string='Is share?')
     short_name = fields.Char(string='Short name')
     display_on_website = fields.Boolean(string='Display on website')
@@ -14,11 +14,17 @@ class ProductTemplate(models.Model):
     by_company = fields.Boolean(string="Can be subscribed by companies?")
     by_individual = fields.Boolean(string="Can be subscribed by individuals?")
     customer = fields.Boolean(string='Become customer')
-    
+
     @api.multi
     def get_web_share_products(self, is_company):
-        if is_company == True:
-            product_templates = self.env['product.template'].search([('is_share','=',True), ('display_on_website','=',True),('by_company','=',True)])
+        if is_company is True:
+            product_templates = self.env['product.template'].search([
+                                        ('is_share', '=', True),
+                                        ('display_on_website', '=', True),
+                                        ('by_company', '=', True)])
         else:
-            product_templates = self.env['product.template'].search([('is_share','=',True), ('display_on_website','=',True),('by_individual','=',True)])
+            product_templates = self.env['product.template'].search([
+                                        ('is_share', '=', True),
+                                        ('display_on_website', '=', True),
+                                        ('by_individual', '=', True)])
         return product_templates
