6dc9b0916791cd4b4db982cdbfc563f72f60d7aa
houssine <houssine@DESKTOP-ICALFFT>
2019-03-26 18:39:39 +0100

    [PEP8] fix pep8 error

--- easy_my_coop_eater/models/coop.py
+++ easy_my_coop_eater/models/coop.py
@@ -1,27 +1,28 @@
 # -*- coding: utf-8 -*-
-from openerp import api, fields, models
+from openerp import api, models
 
-class subscription_request(models.Model):
-    _inherit='subscription.request'
-    
+
+class SubscriptionRequest(models.Model):
+    _inherit = 'subscription.request'
+
     def get_eater_vals(self, partner, share_product_id):
         vals = {}
         eater = share_product_id.eater
-        
+
         if partner.is_company or partner.age < 18:
             eater = 'eater'
-        
+
         vals['eater'] = eater
-        
+
         return vals
-    
+
     @api.one
     def validate_subscription_request(self):
 
-        invoice = super(subscription_request, self).validate_subscription_request()[0]
+        invoice = super(SubscriptionRequest, self).validate_subscription_request()[0]
         partner = invoice.partner_id
-        
+
         vals = self.get_eater_vals(partner, self.share_product_id)
         partner.write(vals)
-        
+
         return invoice
--- easy_my_coop_eater/models/product.py
+++ easy_my_coop_eater/models/product.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-from openerp import api, fields, models, _
+from openerp import fields, models
+
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    
-    eater = fields.Selection([('eater', 'Eater'), ('worker_eater', 'Worker and Eater')], string="Eater/Worker")
+
+    eater = fields.Selection([('eater', 'Eater'),
+                              ('worker_eater', 'Worker and Eater')],
+                             string="Eater/Worker")
