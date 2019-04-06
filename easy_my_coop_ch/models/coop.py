3f256c4f619b1d414a755b8e5672a72608716e58
houssine <houssine@DESKTOP-ICALFFT>
2019-04-06 11:01:53 +0200

    [PEP8] fix pep8

--- easy_my_coop_ch/models/coop.py
+++ easy_my_coop_ch/models/coop.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
-
-from openerp import api, fields, models, _
+from openerp import fields, models
 
 
 class subscription_request(models.Model):
     _inherit = 'subscription.request'
-    
-    company_type = fields.Selection([('ei','Individual company'),
-                                     ('snc','Partnership'),
-                                     ('sa','Limited company (SA)'),
-                                     ('sarl','Limited liability company (Ltd)'),
-                                     ('sc','Cooperative'),
-                                     ('asso','Association'),
-                                     ('fond','Foundation'),
-                                     ('edp','Company under public law')])
-        
+
+    company_type = fields.Selection([('ei', 'Individual company'),
+                                     ('snc', 'Partnership'),
+                                     ('sa', 'Limited company (SA)'),
+                                     ('sarl', 'Limited liability company (Ltd)'), #noqa
+                                     ('sc', 'Cooperative'),
+                                     ('asso', 'Association'),
+                                     ('fond', 'Foundation'),
+                                     ('edp', 'Company under public law')])
+
     def get_required_field(self):
-        required_fields = super(subscription_request,self).get_required_field()
-        if 'no_registre' in required_fields:
-            required_fields.remove('no_registre')
-         
-        return required_fields
+        req_fields = super(subscription_request, self).get_required_field()
+        if 'no_registre' in req_fields:
+            req_fields.remove('no_registre')
+
+        return req_fields
 
     def check_belgian_identification_id(self, nat_register_num):
-        #deactivate number validation for swiss localization
+        # deactivate number validation for swiss localization
         return True
