90e8b077ac119f201b093fba7bd90a2f97faf0c6
houssine <houssine@DESKTOP-ICALFFT>
2019-04-12 16:46:23 +0200

    [IMP] Allow to have different mail template per share.
    This allows to have different mail for "payment received confirmation".
    The mail template can be defined on the share type form. Then on
    reconciliation process, if a specific mail template has been defined it
    will override the default one. otherwise the default mail template will
    be used.

--- easy_my_coop/models/product.py
+++ easy_my_coop/models/product.py
@@ -5,15 +5,17 @@ from openerp import api, fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    is_share = fields.Boolean(string='Is share?')
-    short_name = fields.Char(string='Short name')
-    display_on_website = fields.Boolean(string='Display on website')
-    default_share_product = fields.Boolean(string='Default share product')
-    minimum_quantity = fields.Integer(string='Minimum quantity', default=1)
+    is_share = fields.Boolean(string="Is share?")
+    short_name = fields.Char(string="Short name")
+    display_on_website = fields.Boolean(string="Display on website")
+    default_share_product = fields.Boolean(string="Default share product")
+    minimum_quantity = fields.Integer(string="Minimum quantity", default=1)
     force_min_qty = fields.Boolean(String="Force minimum quantity?")
     by_company = fields.Boolean(string="Can be subscribed by companies?")
     by_individual = fields.Boolean(string="Can be subscribed by individuals?")
-    customer = fields.Boolean(string='Become customer')
+    customer = fields.Boolean(string="Become customer")
+    mail_template = fields.Many2one('mail.template',
+                                    string="Mail template")
 
     @api.multi
     def get_web_share_products(self, is_company):
