7c725518de333228ddc9514e83775a993f713b4d
houssine <houssine@DESKTOP-ICALFFT>
2019-04-16 14:55:26 +0200

    [IMP] override the domain when refund is from release_capital_request

--- easy_my_coop/wizard/__init__.py
+++ easy_my_coop/wizard/__init__.py
@@ -3,3 +3,4 @@ from . import create_subscription_from_partner
 from . import update_partner_info
 from . import validate_subscription_request
 from . import update_share_line
+from . import account_invoice_refund
