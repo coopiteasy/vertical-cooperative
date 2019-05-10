6523ee7f7cc527d8798905ad22cacb9fad5e1b66
houssine <houssine.bakkali@gmail.com>
2019-05-10 16:44:27 +0200

    [LIC] add licence mention

--- easy_my_coop/__manifest__.py
+++ easy_my_coop/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2013-2018 Open Architects Consulting SPRL.
 # Copyright 2018-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
 # - Houssine BAKKALI - <houssine@coopiteasy.be>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).#
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Easy My Coop",
