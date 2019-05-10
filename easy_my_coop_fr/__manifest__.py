7ec34a1324838cc8ee60e8db59c1f99b6c3f6033
houssine <houssine.bakkali@gmail.com>
2019-05-10 16:42:13 +0200

    [MIG] migrate module to v12

--- easy_my_coop_fr/__manifest__.py
+++ easy_my_coop_fr/__manifest__.py
@@ -3,20 +3,21 @@
 # - Houssine BAKKALI - <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).#
 {
-    "name": "Easy My Coop Be",
-    "version": "12.0.1.2.0",
+    "name": "Easy My Coop Fr",
+    "version": "12.0.1.0.0",
     "depends": ["easy_my_coop",
-                "l10n_be",
-                "l10n_be_invoice_bba",
-                ],
+                "l10n_fr"],
     "author": "Houssine BAKKALI <houssine@coopiteasy.be>",
     "category": "Cooperative management",
-    "website": "www.coopiteasy.be",
+    'webstite': "www.coopiteasy.be",
     "license": "AGPL-3",
     "description": """
-    This is the belgian localization for the easy my coop module.
+    This is the french localization for the easy my coop module
     """,
-    'data': [],
+    'data': [
+        'views/subscription_template.xml',
+        # 'views/layout_template.xml',
+        'views/certificate_template.xml'
+    ],
     'installable': True,
-    'application': False,
 }
