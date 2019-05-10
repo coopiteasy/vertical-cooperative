a223d460b606a140a5652482c98c5a9c1426c2c8
houssine <houssine.bakkali@gmail.com>
2019-05-10 19:19:52 +0200

    [MIG] migrate to v12 still wip

---
+++ easy_my_coop_website_portal/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2018-Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Rémy Taymans <remy@coopiteasy.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Easy My Coop Website Portal',
+    "version": "12.0.1.0.0",
+    'depends': [
+        'website',
+        'website_portal_v10',
+        'easy_my_coop',
+        'report',
+    ],
+    'description': """
+        Show cooperator information in the website portal.
+    """,
+    'author': 'Coop IT Easy SCRLfs',
+    'license': 'AGPL-3',
+    'category': 'Cooperative Management',
+    "website": "www.coopiteasy.be",
+    'data': [
+        'views/easy_my_coop_website_portal_templates.xml',
+    ],
+    'installable': False,
+    'application': False,
+}
