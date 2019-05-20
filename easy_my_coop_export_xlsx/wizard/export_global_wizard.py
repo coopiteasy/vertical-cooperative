d909c3d32c2b72e65c5c3945f04c8d3fecedac6e
houssine <houssine.bakkali@gmail.com>
2019-05-20 17:23:33 +0200

    [FIX] fix wrong field name

--- easy_my_coop_export_xlsx/wizard/export_global_wizard.py
+++ easy_my_coop_export_xlsx/wizard/export_global_wizard.py
@@ -125,7 +125,7 @@ class export_global_report(models.TransientModel):
                     worksheet1.write(j, i, line.price_subtotal)
                     i += 1
                 if invoice.payment_ids:
-                    worksheet1.write(j, i, invoice.payment_ids[0].date)
+                    worksheet1.write(j, i, invoice.payment_ids[0].payment_date)
                 i += 1
                 if invoice.subscription_request:
                     ind = len(invoice.subscription_request)-1
