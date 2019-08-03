232d2c2d8c0bb53b7075b69c25b19b629feef1fc
houssine <houssine.bakkali@gmail.com>
2019-08-03 10:44:39 +0200

    [IMP] fix indentation

--- easy_my_coop_be/models/partner.py
+++ easy_my_coop_be/models/partner.py
@@ -5,9 +5,7 @@ from openerp import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    legal_form = fields.Selection(selection_add=[
-                                    ('scrl', 'SCRL'),
-                                    ('asbl', 'ASBL'),
-                                    ('sprl', 'SPRL'),
-                                    ('sa', 'SA')
-                                    ])
+    legal_form = fields.Selection(selection_add=[('scrl', 'SCRL'),
+                                                 ('asbl', 'ASBL'),
+                                                 ('sprl', 'SPRL'),
+                                                 ('sa', 'SA')])
