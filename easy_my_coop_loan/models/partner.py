d7e97aec38c8aa4e4f8827b7cb2c270e07e78783
houssine <houssine.bakkali@gmail.com>
2019-08-20 16:57:39 +0200

    [ADD] bond and loan issues management
    [ADD] bond and loan issues management module skeleton
    
    [IMP] change increase menu sequence
    
    [IMP] add models, fields and views
    
    [IMP] add xml declaration in head of file
    
    [ADD] add easy_my_coop_loan_website - WIP
    
    [IMP] add access rights
    
    [IMP] this raise inconsistency so replace id by default_code.
    
    [IMP] change import openerp to odoo
    
    [IMP] add website loan module
    
    [FIX] put website display in loan
    
    [FIX] fix import
    
    [FIX] fix function
    
    [IMP] use correct name
    
    [IMP] make the loan and bond visible
    
    [IMP] add js, field and logic to set amount limit per subscription
    
    [IMP] remove dependency on recaptcha as user is logged to subscribe
    
    [IMP] add fields
    
    [IMP] save loan issue subscription still in WIP
    
    [IMP] remove alert pop up
    
    [IMP] add dependency to easy_my_coop_website
    
    [IMP] remove force send for sub request creation email notification
    
    [IMP] add mail templates
    
    [IMP] save subscription in the corresponding loan issue. add email notif
    
    [FIX] fix loan issue line view
    
    [FIX] add related field to loan issue. It is where the data stand
    
    [IMP] move term_view up
    
    [FIX] fix js error when false is returned
    
    [FIX] fix function when loan_issue_id in None
    
    [IMP] add actions and button
    
    [FIX] fix action
    
    [FIX] fix mail template
    
    [IMP] set noupdate=1
    
    [IMP] change order
    
    [IMP] display loan issue lines on partner form
    
    [IMP] add loan view in partner form
    
    [IMP] add face value on loan issue and line
    
    add face value on loan issue and line. add as well the quantity and
    computation of the amount
    
    [FIX] missing id overriding values wasn't working
    
    [IMP] getting bond face value and setting it as step.
    
    [IMP] subscribed_amount computed field is the sum of the amount lines
    
    [IMP] allow a waiting payment to be cancelled
    
    [IMP] make field required
    
    [REFACT] move loan issue line code to dedicated file
    
    [IMP] add interest calculation and model
    
    [ADD] bond and loan issues management module skeleton
    
    [IMP] add models, fields and views
    
    [IMP] allow creation by hand
    
    [IMP] adding partner related field
    
    [IMP] put code in separate function
    
    [FIX] pass it to get method
    
    [IMP] routes consistent form
    
    [FIX] fix eof
    
    [FIX] GET is working for ajax call
    
    [IMP] website page for loan issue subscription

---
+++ easy_my_coop_loan/models/partner.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    loan_line_ids = fields.One2many('loan.issue.line',
+                                    'partner_id',
+                                    string="Name")
---
+++ easy_my_coop_loan_website/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2019      Coop IT Easy SCRLfs (<http://www.coopiteasy.be>)
+# - Houssine BAKKALI - <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Easy My Coop Loan Issues Website",
+    "version": "12.0.1.0.1",
+    "depends": [
+        "easy_my_coop_loan",
+        "easy_my_coop_website",
+        "website",
+    ],
+    "author": "Coop IT Easy SCRLfs",
+    "category": "Cooperative management",
+    "website": "http://www.coopiteasy.be",
+    "license": "AGPL-3",
+    "description": """
+    This module implements the subscription page for bonds and
+    subordinated loans.
+    """,
+    'data': [
+        'data/website_loan_data.xml',
+        'template/loan_issue_template.xml'
+    ],
+    'installable': True,
+}
