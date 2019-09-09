ae29f4219dcb94ed9d47c3b91cd82b380c9ac3ea
houssine <houssine.bakkali@gmail.com>
2019-09-09 17:31:53 +0200

    [FIX] fix typo and set back selection_add

--- easy_my_coop_ch/models/coop.py
+++ easy_my_coop_ch/models/coop.py
@@ -4,7 +4,7 @@ from odoo import fields, models
 class SubscriptionRequest(models.Model):
     _inherit = 'subscription.request'
 
-    company_type = fields.Selection([
+    company_type = fields.Selection(selection_add=[
                                     ('ei', 'Individual company'),
                                     ('snc', 'Partnership'),
                                     ('sa', 'Limited company (SA)'),
