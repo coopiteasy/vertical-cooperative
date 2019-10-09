b694520b191f8fc26c419960e5c80fbf28a27dbf
robin.keunen <robin@keunen.net>
2019-10-09 15:46:49 +0200

    [ADD] emc: add licences

--- easy_my_coop/models/operation_request.py
+++ easy_my_coop/models/operation_request.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Houssine Bakkali <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+
 from datetime import datetime
 
 from odoo import api, fields, models, _
