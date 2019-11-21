dcb3c680fd3d03e373fd9dae8581ac432febcbe6
houssine <houssine.bakkali@gmail.com>
2019-11-21 16:54:34 +0100

    [FIX] comment init method

--- easy_my_coop/models/mail_template.py
+++ easy_my_coop/models/mail_template.py
@@ -1,22 +1,12 @@
 from odoo import models, fields
 
-EMAIL_TEMPLATE_IDS = [
-    "easy_my_coop.email_template_release_capital",
-    "easy_my_coop.email_template_confirmation",
-    "easy_my_coop.email_template_confirmation_company",
-    "easy_my_coop.email_template_certificat",
-    "easy_my_coop.email_template_certificat_increase",
-    "easy_my_coop.email_template_share_transfer",
-    "easy_my_coop.email_template_share_update"
-    ]
-
 
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    def init(self):
-        for template_id in EMAIL_TEMPLATE_IDS:
-            mail_template = self.env.ref(template_id)
-            mail_template.easy_my_coop = True
+#     def init(self):
+#         for template_id in EMAIL_TEMPLATE_IDS:
+#             mail_template = self.env.ref(template_id)
+#             mail_template.easy_my_coop = True
 
     easy_my_coop = fields.Boolean(string="Easy my coop mail template")
--- easy_my_coop_loan/models/mail_template.py
+++ easy_my_coop_loan/models/mail_template.py
@@ -1,15 +1,10 @@
 from odoo import models
 
-EMAIL_TEMPLATE_IDS = [
-    "easy_my_coop_loan.loan_subscription_confirmation",
-    "easy_my_coop_loan.loan_issue_payment_request",
-    ]
-
 
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    def init(self):
-        for template_id in EMAIL_TEMPLATE_IDS:
-            mail_template = self.env.ref(template_id)
-            mail_template.easy_my_coop = True
+#     def init(self):
+#         for template_id in EMAIL_TEMPLATE_IDS:
+#             mail_template = self.env.ref(template_id)
+#             mail_template.easy_my_coop = True
