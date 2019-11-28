1e5dd97efd71b1138b6873fc1dd80454a5c149b4
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2019-11-28 15:32:25 +0100

    [FIX] add missing self in super call

--- easy_my_coop_be/models/coop.py
+++ easy_my_coop_be/models/coop.py
@@ -10,18 +10,18 @@ class SubscriptionRequest(models.Model):
                                      ('sa', 'SA')])
 
     def get_partner_company_vals(self):
-        vals = super(SubscriptionRequest).get_partner_company_vals()
+        vals = super(SubscriptionRequest, self).get_partner_company_vals()
         vals['out_inv_comm_algorithm'] = 'random'
         return vals
 
     def get_partner_vals(self):
-        vals = super(SubscriptionRequest).get_partner_vals()
+        vals = super(SubscriptionRequest, self).get_partner_vals()
         vals['out_inv_comm_type'] = 'bba'
         vals['out_inv_comm_algorithm'] = 'random'
         return vals
 
     def get_representative_valst(self):
-        vals = super(SubscriptionRequest).get_representative_vals()
+        vals = super(SubscriptionRequest, self).get_representative_vals()
         vals['out_inv_comm_type'] = 'bba'
         vals['out_inv_comm_algorithm'] = 'random'
         return vals
