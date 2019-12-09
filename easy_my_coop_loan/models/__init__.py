a70ac9cd97e1eb8fdc4290b7dabe0357cf237cf6
houssine <houssine.bakkali@gmail.com>
2019-12-09 17:05:39 +0100

    [LIC] add licensing

--- easy_my_coop_loan/models/__init__.py
+++ easy_my_coop_loan/models/__init__.py
@@ -2,4 +2,3 @@ from . import loan
 from . import loan_issue_line
 from . import interest_line
 from . import partner
-from . import mail_template
--- easy_my_coop_loan/models/interest_line.py
+++ easy_my_coop_loan/models/interest_line.py
@@ -1,3 +1,7 @@
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Houssine BAKKALI <houssine@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
 
 
