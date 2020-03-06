79d76fd5a742471abd21967975bd8f20276b7ec7
enricostano <enricostn@gmail.com>
2020-03-06 22:17:27 +0100

    Add localization for Spain

---
+++ easy_my_coop_es/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Easy My Coop Spain",
+    'version': '12.0.0.0.13',
+    'depends': ['easy_my_coop'],
+    'author': "Coop IT Easy SCRLfs, "
+              "Coopdevs Treball SCCL",
+    'mantainer': "Coopdevs Treball SCCL",
+    'category': "Cooperative management",
+    'description': """
+    Easy My Coop localization for Spain
+    """,
+    "license": "AGPL-3",
+    'data': [
+        "views/subscription_request_view.xml",
+    ],
+    'installable': True,
+}
