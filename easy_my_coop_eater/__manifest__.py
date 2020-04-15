20eab7a654cadee448e60ceecdedca51a0da8844
Vincent Van Rossem <vvrossem@gmail.com>
2020-04-15 14:30:23 +0200

    [IMP] easy_my_coop_eater: license and website

--- easy_my_coop_eater/__manifest__.py
+++ easy_my_coop_eater/__manifest__.py
@@ -1,31 +1,16 @@
-##############################################################################
-#
-#    Copyright (C) 2013-2017 Open Architects Consulting SPRL.
-#    Copyright (C) 2018- Coop IT Easy SCRLfs.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2013-2017 Open Architects Consulting SPRL.
+# Copyright 2018- Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Easy My Coop Eater/worker",
     "version": "12.0.1.0.0",
     "depends": ["easy_my_coop", "partner_age"],
     "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
     "category": "Cooperative management",
-    "website": "www.coopiteasy.be",
-    "description": """
-    This module allows to manage the activity of the cooperator.
+    "website": "https://coopiteasy.be",
+    "summary": """
+    This module allows to manage the activity of the cooperator in the supermarket.
     """,
     "data": ["view/product_view.xml"],
     "installable": True,
---
+++ easy_my_coop_eater/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+    This module allows to manage the activity of the cooperator in the supermarket.
