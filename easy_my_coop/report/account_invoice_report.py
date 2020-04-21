b1d4e3edc75cc3c4eb75e655ff385803e75466f9
robin.keunen <robin@keunen.net>
2020-04-21 18:22:16 +0200

    [REF] OCA pre-commit compliance

--- easy_my_coop/report/account_invoice_report.py
+++ easy_my_coop/report/account_invoice_report.py
@@ -7,7 +7,13 @@ class AccountInvoiceReport(models.Model):
     release_capital_request = fields.Boolean(string="Release capital request")
 
     def _select(self):
-        return super(AccountInvoiceReport, self)._select() + ", sub.release_capital_request as release_capital_request"
+        return (
+            super(AccountInvoiceReport, self)._select()
+            + ", sub.release_capital_request as release_capital_request"
+        )
 
     def _sub_select(self):
-        return super(AccountInvoiceReport, self)._sub_select() + ", ai.release_capital_request as release_capital_request"
+        return (
+            super(AccountInvoiceReport, self)._sub_select()
+            + ", ai.release_capital_request as release_capital_request"
+        )
