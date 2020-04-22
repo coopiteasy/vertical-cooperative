40ad6a4d74d9267df7df077066151095a2532619
robin.keunen <robin@keunen.net>
2020-04-22 14:33:38 +0200

    [REF] pretty js

--- easy_my_coop_loan_website/static/src/js/loan_issue.js
+++ easy_my_coop_loan_website/static/src/js/loan_issue.js
@@ -4,20 +4,20 @@ odoo.define('easy_my_coop_loan_website.oe_easymy_coop_loan', function (require) 
         var ajax = require('web.ajax');
 
         $('.oe_easymy_coop_loan').each(function () {
-	    var oe_easymy_coop_loan = this;
-	    $('#loan_issue').change(function () {
-	    	var loan_issue_id = $("#loan_issue").val();
-	    	ajax.jsonRpc("/subscription/get_loan_issue", 'call', {
+            var oe_easymy_coop_loan = this;
+            $('#loan_issue').change(function () {
+                var loan_issue_id = $("#loan_issue").val();
+                ajax.jsonRpc("/subscription/get_loan_issue", 'call', {
                     'loan_issue_id': loan_issue_id,
-	  		 })
-	  		.then(function (data) {
-	  			if (data !== false) {
-	  				$("#subscription_amount").prop('max', data[loan_issue_id].maximum_amount_per_sub);
-	  				$("#subscription_amount").prop('step', data[loan_issue_id].face_value);
-	  			}
-	        });
-	    });
-	    $('#loan_issue').trigger('change');
+                })
+                    .then(function (data) {
+                        if (data !== false) {
+                            $("#subscription_amount").prop('max', data[loan_issue_id].maximum_amount_per_sub);
+                            $("#subscription_amount").prop('step', data[loan_issue_id].face_value);
+                        }
+                    });
+            });
+            $('#loan_issue').trigger('change');
         });
     });
 });
