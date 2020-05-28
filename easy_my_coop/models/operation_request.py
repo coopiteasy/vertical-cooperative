793c7335e4f2bfe719a29929e3a1a0eb32d123ff
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-05-28 11:56:16 +0200

    fixup! fixup! fixup! [ADD] easy_my_coop: effective date field on operation request

--- easy_my_coop/models/operation_request.py
+++ easy_my_coop/models/operation_request.py
@@ -124,7 +124,9 @@ class OperationRequest(models.Model):
     def _constrain_effective_date(self):
         for obj in self:
             if obj.effective_date and obj.effective_date > fields.Date.today():
-                raise ValidationError("The effective date can not be in the future.")\
+                raise ValidationError(
+                    _("The effective date can not be in the future.")
+                )
 
     @api.multi
     def approve_operation(self):
