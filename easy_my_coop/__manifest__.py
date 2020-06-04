d061f0a0040f5369f74bb6e9f6f610ef97ce5d3e
Houssine BAKKALI <houssine.bakkali@gmail.com>
2020-06-04 21:46:28 +0200

    [PORT] porting improvement from v9
    from https://github.com/coopiteasy/vertical-cooperative/pull/93

--- easy_my_coop/__manifest__.py
+++ easy_my_coop/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Easy My Coop",
     "summary": "Manage your cooperative shares",
-    "version": "12.0.3.0.1",
+    "version": "12.0.3.0.2",
     "depends": [
         "base",
         "web",
