6b1dd2349a85450a7a081d75fb0beaecbe8569ef
robin.keunen <robin@keunen.net>
2020-06-09 14:50:13 +0200

    [FIX] emca: add subscription demo journal

--- easy_my_coop/tests/test_base.py
+++ easy_my_coop/tests/test_base.py
@@ -8,10 +8,6 @@ import odoo.tests.common as common
 class EMCBaseCase(common.TransactionCase):
     def setUp(self):
         super(EMCBaseCase, self).setUp()
-        # todo set from demo data
-        user = self.env["res.users"].browse(self.uid)
-        cooperator_account = self.ref("easy_my_coop.account_cooperator_demo")
-        user.company_id.property_cooperator_account = cooperator_account
 
     def as_user(self):
         self.uid = self.ref("base.user_demo")
