0c505fd58adbd4c82dd32c38e49c7b5b70789706
César López Ramírez <cesar.lopez@coopdevs.org>
2020-06-16 11:31:22 +0200

    Recaptcha option

--- easy_my_coop_website/__manifest__.py
+++ easy_my_coop_website/__manifest__.py
@@ -17,7 +17,9 @@
     """,
     "data": [
         "views/subscription_template.xml",
+        "views/res_config_settings.xml",
         "data/website_cooperator_data.xml",
+        "data/emc_website_recaptcha.xml"
     ],
     "installable": True,
     "application": True,
