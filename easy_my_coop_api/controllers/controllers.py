8cbfb83aae46537aba3e555c9512cd3364fd15e4
robin.keunen <robin@keunen.net>
2020-08-27 18:07:47 +0200

    [FIX] emca: patch for json

--- easy_my_coop_api/controllers/controllers.py
+++ easy_my_coop_api/controllers/controllers.py
@@ -3,9 +3,28 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 
+import re
+
+from odoo import http
 from odoo.http import route
 
 from odoo.addons.base_rest.controllers import main
+
+
+def patch_for_json(path_re):
+    # this is to avoid Odoo, which assumes json always means json+rpc,
+    # complaining about "function declared as capable of handling request
+    # of type 'http' but called with a request of type 'json'"
+    # cf rest-framework/graphql_base/controllers/main.py
+    path_re = re.compile(path_re)
+    orig_get_request = http.Root.get_request
+
+    def get_request(self, httprequest):
+        if path_re.match(httprequest.path):
+            return http.HttpRequest(httprequest)
+        return orig_get_request(self, httprequest)
+
+    http.Root.get_request = get_request
 
 
 class UserController(main.RestController):
@@ -33,3 +52,5 @@ class UserController(main.RestController):
         return self._process_method(
             _service_name, "validate", _id=_id, params=params
         )
+
+    patch_for_json("^/api/subscription-request/[0-9]*/validate$")
