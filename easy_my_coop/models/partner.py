ba0f66446c85af3d42f3211b07e011176635ae5c
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-12-02 19:25:02 +0100

    [FIX] easy_my_coop: respresentatives view depends

--- easy_my_coop/models/partner.py
+++ easy_my_coop/models/partner.py
@@ -200,7 +200,7 @@ class ResPartner(models.Model):
             partner.coop_candidate = is_candidate
 
     @api.multi
-    @api.depends("parent_id", "representative")
+    @api.depends("parent_id", "parent_id.member", "representative")
     def _compute_representative_of_member_company(self):
         for partner in self:
             member_companies = self.env["res.partner"].search(
