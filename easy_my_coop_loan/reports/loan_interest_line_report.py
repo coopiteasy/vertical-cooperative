14b32a855816449afd7c311106f0c401976a94e5
houssine <houssine.bakkali@gmail.com>
2021-05-05 17:18:39 +0200

    [IMP] add report for payment

--- easy_my_coop_loan/reports/loan_interest_line_report.py
+++ easy_my_coop_loan/reports/loan_interest_line_report.py
@@ -10,6 +10,13 @@ class WithholdingTaxDeclarationReport(models.TransientModel):
     _name = "loan.interest.line.report"
     _description = "Loan Interest Line Report"
 
+    report_type = fields.Selection(
+        [("tax", "Withholding Tax Report"),
+         ("reimbursement", "Loan reimbursement Report")
+         ],
+        string="Report type",
+        required=True
+    )
     date_start = fields.Date(string="Start Date")
     date_end = fields.Date(string="End Date")
     interest_lines = fields.Many2many(comodel_name='loan.interest.line')
@@ -23,6 +30,14 @@ class WithholdingTaxDeclarationReport(models.TransientModel):
     )
     total_withholding_tax = fields.Monetary(
         string="Total Withholding Tax",
+        currency_field="company_currency_id",
+    )
+    total_loan_amount = fields.Monetary(
+        string="Total Loan Amount",
+        currency_field="company_currency_id",
+    )
+    total_amount_due = fields.Monetary(
+        string="Total Amount Due",
         currency_field="company_currency_id",
     )
     company_currency_id = fields.Many2one(
@@ -42,7 +57,10 @@ class WithholdingTaxDeclarationReport(models.TransientModel):
     def print_report(self):
         self.ensure_one()
         context = dict(self.env.context)
-        action = self.env.ref(
-            "easy_my_coop_loan.action_withholding_tax_declaration_report"
-        )
+        if self.report_type == "tax":
+            report = "easy_my_coop_loan.action_withholding_tax_declaration_report" # noqa
+        elif self.report_type == "reimbursement":
+            report = "easy_my_coop_loan.action_loan_reimbursement_report"
+        action = self.env.ref(report)
+
         return action.with_context(context).report_action(self, config=False)
