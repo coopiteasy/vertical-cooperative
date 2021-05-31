9aef079129deec3d0b60f823d6bf8408271eb56f
César López Ramírez <cesar.lopez@coopdevs.org>
2021-05-31 15:26:27 +0200

    House number before street

--- easy_my_coop_es_website/__manifest__.py
+++ easy_my_coop_es_website/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Easy My Coop Spain Website",
-    "version": "12.0.0.0.2",
+    "version": "12.0.0.0.3",
     "depends": ["easy_my_coop_es", "easy_my_coop_website"],
     "author": "Coop IT Easy SCRLfs, " "Coopdevs Treball SCCL",
     "mantainer": "Coopdevs Treball SCCL",
