28de49fedbc92e9ba2c13460d24b2f743e482ea0
Daniel Palomar <danypr92@gmail.com>
2021-11-23 09:09:38 +0100

    Remove default key method added in auth_api_key
    We update the version of auth_api_key. Now this module include the
    default api key method to generate a default API key.
    
    Related with: https://github.com/OCA/server-auth/pull/285

--- setup/easy_my_coop_api/setup.py
+++ setup/easy_my_coop_api/setup.py
@@ -3,4 +3,7 @@ import setuptools
 setuptools.setup(
     setup_requires=['setuptools-odoo'],
     odoo_addon=True,
+    odoo_addons={
+        'depends_override': {
+            'auth_api_key': 'odoo12-addon-auth-api-key==12.0.2.1.1',
 )
