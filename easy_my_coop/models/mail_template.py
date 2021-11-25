1beff842ca95a66eed23798d87893b61ecfdf28e
Carmen Bianca Bakker <carmen@coopiteasy.be>
2021-11-25 11:23:26 +0100

    [IMP] easy_my_coop: enable EMC mail template by default on creation
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- easy_my_coop/models/mail_template.py
+++ easy_my_coop/models/mail_template.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    easy_my_coop = fields.Boolean(string="Easy my coop mail template")
+    easy_my_coop = fields.Boolean(string="Easy my coop mail template", default=True)
