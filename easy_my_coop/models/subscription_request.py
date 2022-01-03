1a66e779dc7528be1b1ffce39d5822330d3c6206
robin.keunen <robin@keunen.net>
2022-01-03 14:29:47 +0100

    [REF] easy_my_coop: consistent function names for sending mail

--- easy_my_coop/models/subscription_request.py
+++ easy_my_coop/models/subscription_request.py
@@ -75,7 +75,7 @@ class SubscriptionRequest(models.Model):
                     % request.share_product_id.name
                 )
 
-    def _send_confirmation_email(self):
+    def _send_confirmation_mail(self):
         if self.company_id.send_confirmation_email:
             mail_template_notif = self.get_mail_template_notif(
                 is_company=self.partner_id.is_company
@@ -107,7 +107,7 @@ class SubscriptionRequest(models.Model):
             cooperator.write({"cooperator": True})
 
         subscription_request = super(SubscriptionRequest, self).create(vals)
-        subscription_request._send_confirmation_email()
+        subscription_request._send_confirmation_mail()
         return subscription_request
 
     @api.model
@@ -122,7 +122,7 @@ class SubscriptionRequest(models.Model):
                 vals = self.is_member(vals, cooperator)
                 vals["partner_id"] = cooperator.id
         subscription_request = super(SubscriptionRequest, self).create(vals)
-        subscription_request._send_confirmation_email()
+        subscription_request._send_confirmation_mail()
         return subscription_request
 
     def check_empty_string(self, value):
@@ -555,7 +555,7 @@ class SubscriptionRequest(models.Model):
 
         # validate the capital release request
         invoice.action_invoice_open()
-        invoice.send_capital_release_request_email()
+        invoice.send_capital_release_request_mail()
 
         return invoice
 
@@ -759,7 +759,7 @@ class SubscriptionRequest(models.Model):
             raise ValidationError(_("You cannot cancel a request in this " "state."))
         self.write({"state": "cancelled"})
 
-    def _send_waiting_list_email(self):
+    def _send_waiting_list_mail(self):
         if self.company_id.send_waiting_list_email:
             waiting_list_mail_template = self.env.ref(
                 "easy_my_coop.email_template_waiting_list", False
@@ -773,5 +773,5 @@ class SubscriptionRequest(models.Model):
             raise ValidationError(
                 _("Only draft request can be put on the waiting list.")
             )
-        self._send_waiting_list_email()
+        self._send_waiting_list_mail()
         self.write({"state": "waiting"})
