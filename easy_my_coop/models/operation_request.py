8953266bc474c4934d4cc8582b8290ef2f93926d
robin.keunen <robin@keunen.net>
2022-01-03 14:29:47 +0100

    [REF] easy_my_coop: consistent function names for sending mail

--- easy_my_coop/models/operation_request.py
+++ easy_my_coop/models/operation_request.py
@@ -288,14 +288,14 @@ class OperationRequest(models.Model):
     def _get_share_update_mail_template(self):
         return self.env.ref("easy_my_coop.email_template_share_update", False)
 
-    def _send_share_transfer_email(
+    def _send_share_transfer_mail(
         self, sub_register_line
     ):  # fixme unused argument is used in synergie project. Do not remove.
         if self.company_id.send_share_transfer_email:
             cert_email_template = self._get_share_transfert_mail_template()
             cert_email_template.send_mail(self.partner_id_to.id, False)
 
-    def _send_share_update_email(
+    def _send_share_update_mail(
         self, sub_register_line
     ):  # fixme unused argument is used in synergie project. Do not remove.
         if self.company_id.send_share_update_email:
@@ -414,6 +414,6 @@ class OperationRequest(models.Model):
 
         # send mail to the receiver
         if self.operation_type == "transfer":
-            self._send_share_transfer_email(sub_register_line)
+            self._send_share_transfer_mail(sub_register_line)
 
-        self._send_share_update_email(sub_register_line)
+        self._send_share_update_mail(sub_register_line)
