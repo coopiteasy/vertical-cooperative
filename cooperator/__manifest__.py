bd1505f72829ca8f7ea4ee2f697ad396edc66ff5
robin.keunen <robin@keunen.net>
2022-01-28 16:38:13 +0100

    [MIG] cooperator: set noupdate to false on report actions

--- cooperator/__manifest__.py
+++ cooperator/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Cooperators",
     "summary": "Manage your cooperators",
-    "version": "12.0.4.0.0",
+    "version": "12.0.4.0.1",
     "depends": [
         "base",
         "web",
