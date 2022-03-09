7d03349981e1a2233c0a628d969c2ee88709b47f
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-03-09 13:38:35 +0100

    [MOV] easy_my_coop_api_logs → cooperator_api_logs
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- cooperator_api_logs/__manifest__.py
+++ cooperator_api_logs/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Easy My Coop API Logs",
+    "name": "Cooperators API Logs",
     "version": "12.0.0.0.1",
     "depends": [],
     "author": "Coop IT Easy SCRLfs",
