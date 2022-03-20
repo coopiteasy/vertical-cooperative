d30231a7891fedebc4e9eee28d0a74b1ebfc700d
hugues de keyzer <odoo@hugues.info>
2022-03-20 15:04:24 +0100

    [REL] easy_my_coop 12.0.3.3.0

--- easy_my_coop/__manifest__.py
+++ easy_my_coop/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Easy My Coop",
     "summary": "Manage your cooperative shares",
-    "version": "12.0.3.2.0",
+    "version": "12.0.3.3.0",
     "depends": [
         "base",
         "web",
