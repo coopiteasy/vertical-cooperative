f93fc2460e66bbfa9e55361f4424adef109c4052
hugues de keyzer <odoo@hugues.info>
2022-03-20 15:05:15 +0100

    [REL] easy_my_coop_ch 12.0.1.2.0

--- easy_my_coop_ch/__manifest__.py
+++ easy_my_coop_ch/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Easy My Coop Switzerland",
     "summary": "Easy My Coop Switzerland localization",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.2.0",
     "license": "AGPL-3",
     "depends": ["easy_my_coop", "easy_my_coop_website", "l10n_ch"],
     "author": "Coop IT Easy SCRLfs",
