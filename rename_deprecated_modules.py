6ef322fd4b8abe5a7b1f974dead4e4d4ff1a458d
robin.keunen <robin@keunen.net>
2022-03-24 15:46:28 +0100

    [IMP] upadte rename_deprecated_modules comments

--- rename_deprecated_modules.py
+++ rename_deprecated_modules.py
@@ -13,44 +13,31 @@ env = env  # noqa: F821
 # Comments are there to prevent merge conflicts. Put the dictionary entry UNDER
 # the comment of the respective module.
 renamed_modules = {
-    # easy_my_coop                  => ok
     "easy_my_coop": "cooperator",
-    # easy_my_coop_api              => ok
     "easy_my_coop_api": "connector_api",
-    # easy_my_coop_api_logs         => ok
     "easy_my_coop_api_logs": "connector_api_logs",
-    # easy_my_coop_be               => ok
     "easy_my_coop_be": "l10n_be_cooperator",
-    # easy_my_coop_ch               => ok
     "easy_my_coop_ch": "l10n_ch_cooperator",
-    # easy_my_coop_connector
-    # "easy_my_coop_connector": "cooperator_connector",
+    # easy_my_coop_connector        => waiting for financité
     # easy_my_coop_dividend         => do not move to oca, not installable
-    # easy_my_coop_es
     "easy_my_coop_es": "l10n_es_cooperator",
-    # easy_my_coop_es_website       => coopdevs
     "easy_my_coop_es_website": "l10n_es_cooperator",
     # easy_my_coop_export_xlsx      => do not move to oca, not installable
-    # easy_my_coop_fr               => ok
     "easy_my_coop_fr": "l10n_fr_cooperator",
     # easy_my_coop_loan             => do not move to oca, seldom used
     # easy_my_coop_loan_account     => idem
     # easy_my_coop_loan_account_be  => idem
     # easy_my_coop_loan_bba         => idem
     # easy_my_coop_loan_website     => idem
-    # easy_my_coop_payment_term
     "easy_my_coop_payment_term": "cooperator",
-    # easy_my_coop_taxshelter_report => ok
     "easy_my_coop_taxshelter_report": "l10n_be_cooperator",
     # easy_my_coop_website
     # "easy_my_coop_website": "cooperator",
-    # easy_my_coop_website_portal   => ok
     "easy_my_coop_website_portal": "cooperator_portal",
-    # easy_my_coop_website_taxshelter
     "easy_my_coop_website_taxshelter": "l10n_be_cooperator_portal",
     # partner_age                   => dependency removed, move to addons
-    # theme_light                   => remove from dependencies
-    # website_recaptcha_reloaded    => move to addons
+    # theme_light                   => removed from dependencies
+    # website_recaptcha_reloaded    => moved to addons
 }
 
 _logger.info("rename easy_my_coop_x modules to cooperator_x")
