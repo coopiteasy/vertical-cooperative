29f7b1153c19fc16d176b1a2ab11f0051334f324
robin.keunen <robin@keunen.net>
2022-03-28 11:49:51 +0200

    [REF] rename cooperator.subscription_request_form
    wip

---
+++ cooperator/migrations/12.0.5.0.0/pre-migration.py
@@ -0,0 +1,19 @@
+import logging
+
+# for this major refactoring, we decided to depend on
+# openupgradelib
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__ + " 12.0.5.0.0")
+
+renamed_xml_ids = (
+    (
+        "cooperator.subscription_request_form",
+        "cooperator.subscription_request_view_form",
+    ),
+)
+
+
+def migrate(cr, version):
+    _logger.info("renaming xml_ids")
+    openupgrade.rename_xmlids(cr, renamed_xml_ids)
