5d76c1c81d152f3e50cd859440f191a2731dc866
Victor Champonnois <victor@coopiteasy.be>
2022-04-01 14:29:33 +0200

    [REF] recaptcha depends on portal rather than website

--- website_recaptcha/models/res_config.py
+++ website_recaptcha/models/res_config.py
@@ -1,30 +1,27 @@
 # Copyright 2019 Simone Orsi - Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
-class WebsiteConfigSettings(models.TransientModel):
+class PortalConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    recaptcha_key_site = fields.Char(
-        related="website_id.recaptcha_key_site", readonly=False
-    )
-    recaptcha_key_secret = fields.Char(
-        related="website_id.recaptcha_key_secret", readonly=False
-    )
+    recaptcha_key_site = fields.Char()
+    recaptcha_key_secret = fields.Char()
+
     has_google_recaptcha = fields.Boolean(
         "Google reCaptcha",
         compute="_compute_has_google_recaptcha",
         inverse="_inverse_has_google_recaptcha",
         readonly=False,
     )
+    # "?????"
+    # @api.depends("website_id")
+    # def _compute_has_google_recaptcha(self):
+    #     self.has_google_recaptcha = bool(self.recaptcha_key_site)
 
-    @api.depends("website_id")
-    def _compute_has_google_recaptcha(self):
-        self.has_google_recaptcha = bool(self.recaptcha_key_site)
-
-    def _inverse_has_google_recaptcha(self):
-        if not self.has_google_recaptcha:
-            self.recaptcha_key_site = False
-            self.recaptcha_key_secret = False
+    # def _inverse_has_google_recaptcha(self):
+    #     if not self.has_google_recaptcha:
+    #         self.recaptcha_key_site = False
+    #         self.recaptcha_key_secret = False
