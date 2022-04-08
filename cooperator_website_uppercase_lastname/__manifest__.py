5b5ddda6bfeef1f21c9a99422353390aae1d7068
robin.keunen <robin@keunen.net>
2022-04-08 16:30:20 +0200

    [ADD] cooperator_website_uppercase_lastname

---
+++ cooperator_website_uppercase_lastname/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Coop IT Easy SCRLfs
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Cooperator Uppercase Last Name",
+    "summary": """
+        This module UPPERCASES the last names of submitted requests""",
+    "version": "12.0.1.0.0",
+    "category": "Cooperative management",
+    "website": "https://coopiteasy.be",
+    "author": "Coop IT Easy SCRLfs",
+    "license": "AGPL-3",
+    "application": False,
+    "depends": ["easy_my_coop_website"],
+}
