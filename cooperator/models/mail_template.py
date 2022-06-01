2e57365b910c7a870e7dc59903dc6fdfa0dc6c46
robin.keunen <robin@keunen.net>
2022-06-01 10:01:46 +0200

    Revert "Revert "Merge pull request #267 from coopiteasy/12.0-cooperator-xml-ids""
    This reverts commit 3e044d6203dcc311ff43e1b0da98e290c09c13e1.

--- cooperator/models/mail_template.py
+++ cooperator/models/mail_template.py
@@ -4,4 +4,5 @@ from odoo import fields, models
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
+    # fixme
     easy_my_coop = fields.Boolean(string="Easy my coop mail template")
