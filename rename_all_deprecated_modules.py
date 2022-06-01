17b162a8c3bcb07b18937a41d59e04ee39606dbe
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-01 10:10:23 +0200

    [ADD] Helper migration scripts
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ rename_all_deprecated_modules.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Coop IT Easy SCRL fs
+#   Carmen Bianca Bakker <carmen@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+"""CIE-specific script to run migration on all test databases. This script may
+not work for you if your server is different.
+"""
+
+import datetime
+import logging
+import subprocess
+
+_logger = logging.getLogger(__name__)
+
+
+def all_databases():
+    result = subprocess.run(["ociedoo", "list-db"], check=True, stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
+    return [line for line in output.splitlines() if line]
+
+
+def filter_databases(databases):
+    return [database for database in databases if database.endswith("-test")]
+
+
+def now():
+    return datetime.datetime.now().strftime("%Y-%m-%dT%H-%m")
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    databases = filter_databases(all_databases())
+    for database in databases:
+        new_database = f"{database}-renamedep-{now()}"
+        _logger.info(f"Creating {new_database} from {database}")
+        subprocess.run(["ociedoo", "copy-db", database, new_database], check=True)
+        subprocess.run(["./rename_deprecated_modules.sh", new_database], check=True)
+
+
+if __name__ == "__main__":
+    main()
