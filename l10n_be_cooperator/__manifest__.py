454f05b5219403c6d0b3d99675c46322b456ac23
coccinouille <victor@coopiteasy.be>
2022-06-01 09:55:29 +0200

    [REF] easy_my_coop_be -> l10n_be_cooperator

--- l10n_be_cooperator/__manifest__.py
+++ l10n_be_cooperator/__manifest__.py
@@ -3,10 +3,15 @@
 # - Houssine BAKKALI - <houssine@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).#
 {
-    "name": "Easy My Coop Belgium",
-    "summary": "Easy My Coop Belgium Localization",
+    "name": "Cooperators Belgium",
+    "summary": "Cooperators Belgium Localization",
     "version": "12.0.1.3.0",
-    "depends": ["cooperator", "l10n_be", "l10n_be_invoice_bba"],
+    "depends": [
+        "cooperator",
+        "easy_my_coop_website",
+        "l10n_be",
+        "l10n_be_invoice_bba",
+    ],
     "author": "Coop IT Easy SCRLfs",
     "category": "Cooperative management",
     "website": "https://coopiteasy.be",
