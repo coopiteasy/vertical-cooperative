9e5ddff468d82902151fdf4a90091b91616bee4f
robin.keunen <robin@keunen.net>
2022-06-01 09:43:55 +0200

    [MIG] rename deprecated modules script

---
+++ rename_deprecated_modules.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+env = env  # noqa F821 undefined name 'env'
+
+renamed_modules = {
+    "easy_my_coop": "cooperator",
+}
+
+logging.info("rename easy_my_coop_x modules to cooperator_x")
+openupgrade.update_module_names(env.cr, renamed_modules.items())
+env.cr.commit()
