96ab5b36d5297cf37c0d6fdf2ecfac83b8ba7c3b
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-01 10:10:23 +0200

    [ADD] Helper migration scripts
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ rename_all_deprecated_modules.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Copyright 2021 Coop IT Easy SCRL fs
+#   Carmen Bianca Bakker <carmen@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+"""CIE-specific script to run migration on all test databases. This script may
+not work for you if your server is different.
+"""
+
+import logging
+import os
+import pathlib
+import subprocess
+
+_logger = logging.getLogger(__name__)
+
+
+def all_databases():
+    result = subprocess.run(["ociedoo", "list-db"], check=True, stdout=subprocess.PIPE)
+    output = result.stdout.decode("utf-8")
+    return [line for line in output.splitlines() if line]
+
+
+def filter_databases(databases):
+    return [database for database in databases if database.endswith("-test")]
+
+
+def main():
+    os.chdir(pathlib.Path(__file__).parent)
+    logging.basicConfig(level=logging.INFO)
+    databases = filter_databases(all_databases())
+    for database in databases:
+        new_database = f"{database}-renamedep"
+        _logger.info(f"Removing {new_database} if it exists")
+        subprocess.run(["ociedoo", "drop-db", new_database])
+        _logger.info(f"Creating {new_database} from {database}")
+        subprocess.run(["ociedoo", "copy-db", database, new_database], check=True)
+        _logger.info(f"Running renaming migration on {new_database}")
+        subprocess.run(["./rename_deprecated_modules.sh", new_database], check=True)
+        _logger.info(f"Done with renaming migration of {new_database}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ rename_deprecated_modules.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Usage: rename_depreacted_modules.sh database-name-here
+#
+# Super simple script that enables less typing.
+
+cd $(dirname "$0")
+
+cat rename_deprecated_modules.py | ~/venv/bin/odoo shell --config ~/odoo.conf --logfile /dev/stdout --stop-after-init -d $1
