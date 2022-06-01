f9750c8081c67034fc195fe0d35a6b009bdffef6
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-01 10:10:23 +0200

    [FIX] cooperator_website: Manifest name
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- cooperator_website/__manifest__.py
+++ cooperator_website/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Cooperator Website",
+    "name": "Cooperators Website",
     "version": "12.0.3.0.0",
     "depends": [
         "cooperator",
