758e9dc498ecbe8c04799a0057f4c4d26bfe41ec
robin.keunen <robin@keunen.net>
2022-06-01 10:01:46 +0200

    [REF] cooperator: rename field easy_my_coop on mail.template

--- cooperator/models/mail_template.py
+++ cooperator/models/mail_template.py
@@ -4,5 +4,4 @@ from odoo import fields, models
 class MailTemplate(models.Model):
     _inherit = "mail.template"
 
-    # fixme
-    easy_my_coop = fields.Boolean(string="Easy my coop mail template")
+    is_cooperator_template = fields.Boolean(string="Cooperator mail template")
