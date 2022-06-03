6ff4e389980c6b94af8df377f2fc68197c2a76a7
Victor Champonnois <victor@coopiteasy.be>
2022-06-03 09:48:06 +0200

    [FIX] uninstall partner_age in rename_dep_modules

--- rename_deprecated_modules.py
+++ rename_deprecated_modules.py
@@ -43,3 +43,10 @@ renamed_modules = {
 _logger.info("rename easy_my_coop_x modules to cooperator_x")
 openupgrade.update_module_names(env.cr, renamed_modules.items(), merge_modules=True)
 env.cr.commit()
+
+modules_to_uninstall = [
+    "partner_age",
+]
+for module in modules_to_uninstall:
+    _logger.info("uninstall %s" % module)
+    env["ir.module.module"].search([("name", "=", module)]).button_immediate_uninstall()
