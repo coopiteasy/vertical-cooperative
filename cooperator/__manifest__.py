ba96249d3b0a24947661fc4e3bb4e9ef4e286c3e
Victor Champonnois <victor@coopiteasy.be>
2022-06-08 09:46:44 +0200

    [FIX] duplicated dependency in cooperator

--- cooperator/__manifest__.py
+++ cooperator/__manifest__.py
@@ -15,7 +15,6 @@
         "account",
         "base_iban",
         "product",
-        "partner_contact_birthdate",
         "partner_firstname",
         "partner_contact_birthdate",
         "partner_contact_address",
