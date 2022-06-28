6815b5830302012ddf71bf435981c6604222742b
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-06-28 16:41:34 +0200

    [14.0][MIG] cooperator_portal: Migration to 14.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- cooperator_portal/__manifest__.py
+++ cooperator_portal/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Cooperator Portal",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["cooperator", "website", "account", "portal"],
     "summary": """
         Show cooperator information in the website portal.
