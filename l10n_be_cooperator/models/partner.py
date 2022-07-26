4eccc0ac0eaa640bea46f59a3d14cdf2b19ec028
Victor Champonnois <victor@coopiteasy.be>
2022-07-26 13:19:38 +0200

    [MIG] L10n_be_cooperator: Migration to 14.0

--- l10n_be_cooperator/models/partner.py
+++ l10n_be_cooperator/models/partner.py
@@ -10,5 +10,11 @@ class ResPartner(models.Model):
             ("asbl", "ASBL"),
             ("sprl", "SPRL"),
             ("sa", "SA"),
-        ]
+        ],
+        ondelete={
+            "scrl": "set null",
+            "asbl": "set null",
+            "sprl": "set null",
+            "sa": "set null",
+        },
     )
