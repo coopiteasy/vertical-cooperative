b84110f485e49004084cce3a8672bd20327854b6
hugues de keyzer <odoo@hugues.info>
2022-08-03 17:44:21 +0200

    [FIX] fix eslint error

--- cooperator_website/static/src/js/cooperator.js
+++ cooperator_website/static/src/js/cooperator.js
@@ -1,11 +1,9 @@
 odoo.define("cooperator.oe_cooperator", function (require) {
+    "use strict";
     $(document).ready(function () {
-        // "use strict";
         var ajax = require("web.ajax");
 
         $(".oe_cooperator").each(function () {
-            "use strict";
-
             var oe_cooperator = this;
 
             $("#share_product_id").change(function () {
