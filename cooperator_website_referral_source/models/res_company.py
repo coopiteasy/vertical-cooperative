d6efd06db59703044bd9982a43de24bf0e1dac62
Victor Champonnois <victor@coopiteasy.be>
2022-09-22 09:45:27 +0200

    fixup: remove setting to activate referral_source

--- cooperator_website_referral_source/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-    activate_referral_source = fields.Selection(
-        [],
-        "Activate field: 'How did you know about us?'",
-    )
