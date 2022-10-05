9c6244900caa334f8beb0a70d5636fbd604058e5
hugues de keyzer <odoo@hugues.info>
2022-10-05 15:08:12 +0200

    [REM] l10n_be_cooperator
    move to oca's cooperative repository.

--- l10n_be_cooperator/models/subscription_request.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-from . import partner
-
-
-class SubscriptionRequest(models.Model):
-    _inherit = "subscription.request"
-
-    company_type = fields.Selection(selection_add=partner.get_company_type_selection())
--- l10n_be_cooperator/readme/DEVELOP.rst
+++
@@ -1,2 +0,0 @@
-Do not implement tests before fixing the direct use of self.env.cr.commit()
-in models.tax_shelter_declaration
