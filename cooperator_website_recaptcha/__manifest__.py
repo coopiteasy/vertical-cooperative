0f8aed127a809b87afd71c7679d7d89f5246c5d6
hugues de keyzer <odoo@hugues.info>
2022-12-09 14:06:49 +0100

    [REF] refactor cooperator_website_recaptcha
    * move the recaptcha widget view to portal_recaptcha.
    * remove res.company.captcha_type (portal_recaptcha can now be enabled
      and disabled in the settings).
    * override WebsiteSubscription._additional_validate() instead of
      .validation().

--- cooperator_website_recaptcha/__manifest__.py
+++ cooperator_website_recaptcha/__manifest__.py
@@ -2,24 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Cooperators Website Recaptcha",
-    "summary": """
-        Add Google Recaptcha to Subscription Request Form""",
-    "version": "14.0.1.0.0",
+    "name": "Cooperators Website reCAPTCHA",
+    "summary": "Add reCAPTCHA to Subscription Request Form",
+    "version": "14.0.1.1.0",
     "category": "Cooperative management",
     "website": "https://coopiteasy.be",
     "author": "Coop IT Easy SC",
     "license": "AGPL-3",
-    "application": False,
     "depends": [
         "cooperator_website",
         "portal_recaptcha",
     ],
-    "excludes": [],
     "data": [
-        "views/res_company_views.xml",
         "views/subscription_template.xml",
     ],
-    "demo": [],
-    "qweb": [],
 }
